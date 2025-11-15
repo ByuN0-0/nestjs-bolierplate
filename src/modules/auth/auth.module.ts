3ccf8e6c74cbfa19c83d29c49b51e1a53290908f
byun00 <byhwang@blynx.co>
2025-11-15 17:06:24 +0900

    feat: enhance authentication module with dynamic JWT configuration
    - Added support for dynamic JWT secret and expiration settings.
    - Integrated AuthModule into OAuthModule for improved authentication flow.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtSignOptions } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt.secret'),
+        secret:
+          configService.get<string>('jwt.accessSecret') ??
+          'please-set-a-secret',
         signOptions: {
-          expiresIn: configService.get<number>('jwt.expiresIn'),
+          expiresIn:
+            (configService.get(
+              'jwt.accessExpiresIn',
+            ) as JwtSignOptions['expiresIn']) ?? '1h',
         },
       }),
       inject: [ConfigService],
--- src/modules/oauth/oauth.module.ts
+++ src/modules/oauth/oauth.module.ts
@@ -6,11 +6,13 @@ import { OAuthController } from './oauth.controller';
 import { UserModule } from '../user/user.module';
 import { oauthStrategyProviders } from './oauth.providers';
 import { OAuthProviderGuard } from './guards/oauth-provider.guard';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     PassportModule.register({ session: false }),
     ConfigModule,
+    AuthModule,
     UserModule,
   ],
   providers: [OAuthService, ...oauthStrategyProviders, OAuthProviderGuard],
